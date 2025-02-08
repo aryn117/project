bcc292498a0e0cab31c1d136b11136a2a2a77670
Aryan Sharma <72523773+aryn117@users.noreply.github.com>
2025-02-08 18:01:13 +0530

    etjeerheth

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,33 @@
 import { Link, useLocation } from 'react-router-dom';
+import { RxHamburgerMenu } from "react-icons/rx";
 
 function Navbar() {
   const location = useLocation();
-  
+
   return (
-    <div className="relative shadow-lg navbar bg-base-100">
+    <div className="shadow-lg navbar bg-base-100">
       <div className="container mx-auto">
         <div className="flex-1">
           <Link to="/" className="font-mono text-xl font-bold">TorrentHub</Link>
         </div>
         <div className="flex-none">
-        <details>
-          <summary className='text-xl cursor-pointer' >Menu</summary>
-          <ul class="bg-base-100  rounded-t-none absolute right-5 p-2">          
-            <li className='px-2 py-1 my-1 rounded-md bg-base-200 hover:bg-base-300' ><Link to="/" className={location.pathname === '/' ? 'active' : ''}>Search</Link></li>
-            <li className='px-2 my-1 rounded-md y-1 a bg-base-200 hover:bg-base-300' ><Link to="/trending" className={location.pathname === '/trending' ? 'active' : ''}>Trending</Link></li>
-            <li className='px-2 py-1 my-1 rounded-md bg-base-200 hover:bg-base-300' ><Link to="/recent" className={location.pathname === '/recent' ? 'active' : ''}>Recent</Link></li>
-            <li className='px-2 py-1 my-1 rounded-md bg-base-200 hover:bg-base-300' ><Link to="/settings" className={location.pathname === '/settings' ? 'active' : ''}>Settings</Link></li>
-         
-          </ul>
-        </details>
-         
+      
+
+          {/* ========================================================= */}
+          <div className="dropdown dropdown-end">
+            <div tabIndex={0} role="button" className="m-1 text-xl btn "><RxHamburgerMenu /></div>
+            <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 py-4 px-3 shadow">
+              <li className='my-1' ><Link to="/" className={location.pathname === '/' ? 'active' : ''}>Search</Link></li>
+              <li className='my-1' ><Link to="/trending" className={location.pathname === '/trending' ? 'active' : ''}>Trending</Link></li>
+              <li className='my-1' ><Link to="/recent" className={location.pathname === '/recent' ? 'active' : ''}>Recent</Link></li>
+              <li className='my-1' ><Link to="/settings" className={location.pathname === '/settings' ? 'active' : ''}>Settings</Link></li>
+            </ul>
+          </div>
+          {/* ========================================================= */}
+
+
+
+
         </div>
       </div>
     </div>
